706e52147085c051ac26d673a5c633ce1f0656ea
Cal Krug <cal.krug@protonmail.com>
2021-12-11 00:15:29 -0600

    Attempt: JSON, card rendering, useEffect

--- react-meta-component/src/App.js
+++ react-meta-component/src/App.js
@@ -1,10 +1,17 @@
-import logo from './assets/favicon.svg';
+import React from 'react';
 import './App.css';
+import cards from './data/cards.json';
+import { useState } from "react";
 
 function App() {
+  const [cardState, setCardState] = useState(cards.image);
+
   return (
     <div className="App">
-      <p>Lorem ipsum</p>
+      <div className="cards">
+        This card is a(n) <img src={cards.image}></img>.
+      </div>
+      <button onClick={() => setCardState("King")}></button>
     </div>
   );
 }

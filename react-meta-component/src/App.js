25d3d8509523fcf362ca972ca07ff9f6a4aa883f
Cal Krug <cal.krug@protonmail.com>
2021-12-13 11:07:30 -0600

    card display

--- react-meta-component/src/App.js
+++ react-meta-component/src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import {React, useEffect} from 'react';
 import './App.css';
-import deck from './data/deck.json';
 import { useState } from "react";
-
+import Game from './data/game.json';
 import cardImage from "./assets/favicon.svg";
 
 function App() {
-  const [cardState, setCardState] = useState(deck.image);
+  const [gameState, setGameState] = useState({});
+
+  useEffect(() => {
+    async function fetchData(url) {
+      const response = await fetch(url);
+      const data = await response.json();
+      setGameState(data);
+    }
+
+    fetchData("./src/data/game.json");
+  }, []);
+
+  let cardDisplay = () => {
+    if (gameState.cards && gameState.cards.length > 0) {
+      return <div><img src={gameState.cards[0]} /></div>
+    }
+    else {
+      return <div>Ain't no cards here!</div>
+    }
+  } 
 
   return (
     <div className="App">
-      <div className="deck">
-        This card is a(n) <img src={cardImage}></img>.
-      </div>
-      <button onClick={() => setCardState("King")}></button>
+      {cardDisplay()}
     </div>
   );
 }

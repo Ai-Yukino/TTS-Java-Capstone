346c7e78471f3554caefd72eccd2a6155bb88f7c
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-12-15 23:15:39 -0600

    🤏 Small changes

--- react-meta-component/src/routes/Game.js
+++ react-meta-component/src/routes/Game.js
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
 import Card from "../components/small/Card";
 
-export default function game() {
-  return <div>Game display:</div>;
+export default function Game() {
+  // 📝 Initialize state so VSCode doesn't cry
+  const [shuffledDeck, setShuffledDeck] = useState([
+    {
+      id: -1,
+      Suit: "",
+      Rank: "",
+      BlackjackValue: -1,
+      Image: { src: "", alt: "" },
+    },
+  ]);
+
+  useEffect(() => {
+    async function fetchData(url) {
+      const response = await fetch(url);
+      const data = await response.json();
+      setShuffledDeck(data);
+    }
+    fetchData("Data/Testing/shuffledDeck.json");
+  }, []);
+
+  const baseUrl = "Images/";
+
+  return (
+    <div>
+      <Card
+        src={baseUrl + shuffledDeck[99].Image.src}
+        alt={baseUrl + shuffledDeck[99].Image.alt}
+      />
+    </div>
+  );
 }

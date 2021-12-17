0a04c75c2283754cb0744c3607c4a08a5faf99f7
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-12-17 01:06:11 -0600

    ➕ Started player and dealer display components

---
+++ react-meta-component/src/components/DealerDisplay.js
@@ -0,0 +1,13 @@
+export default function DealerDisplay({
+  cardHand = [],
+  PlayerActions = {
+    insuranceBet: false,
+    stand: false,
+    hit: false,
+    doubleDown: false,
+    split: false,
+    surrender: false,
+  },
+}) {
+  return <div />;
+}

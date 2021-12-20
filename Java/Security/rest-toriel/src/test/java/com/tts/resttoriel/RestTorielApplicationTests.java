67586bb67d6ae8e17e5365a6a5fa974ebec9edbf
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-12-19 22:28:08 -0600

    ➕ Created Security folder

---
+++ Java/Security/rest-toriel/src/test/java/com/tts/resttoriel/RestTorielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.resttoriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTorielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

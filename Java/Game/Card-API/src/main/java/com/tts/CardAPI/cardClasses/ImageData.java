a6763096a31029f7730e94f4b987ca6294b3fca5
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-12-20 01:55:46 -0600

    ➕ Added new Card-API codebase

---
+++ Java/Game/Card-API/src/main/java/com/tts/CardAPI/cardClasses/ImageData.java
@@ -0,0 +1,19 @@
+package com.tts.CardAPI.cardClasses;
+
+public class ImageData {
+    private final String src;
+    private final String alt;
+
+    ImageData(String url, String altText) {
+        src = url;
+        alt = altText;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public String getAlt() {
+        return alt;
+    }
+}

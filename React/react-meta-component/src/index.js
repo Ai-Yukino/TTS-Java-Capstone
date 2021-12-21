f7cf8c512c718819cf92c512eaddecd1eba3fb83
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-12-21 11:33:00 -0600

    🔗 Added routing

--- React/react-meta-component/src/index.js
+++ React/react-meta-component/src/index.js
@@ -2,14 +2,21 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
+
 import Game from "./routes/Game";
+import HowToPlay from "./routes/HowToPlay";
+import Login from "./routes/Login";
+import Menu from "./routes/Menu";
 import Results from "./routes/Results";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Game />} />
-      {/* <Results /> */}
+      <Route path="/" element={<Menu />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="/how-to" element={<HowToPlay />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/results" element={<Results />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")

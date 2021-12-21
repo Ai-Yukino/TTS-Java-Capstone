74f951199a1cc52ac592ac2a9fdcfe74dd3968f6
Cal Krug <cal.krug@protonmail.com>
2021-12-21 09:33:13 -0600

    update

--- React/react-meta-component/src/index.js
+++ React/react-meta-component/src/index.js
@@ -2,12 +2,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
+// We still need to import the rest of the route compoments
 import Game from "./routes/Game";
+import Menu from "./routes/Menu";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
+      {/* We still need to route the rest of the route compoments btw */}
       <Route path="/" element={<Game />} />
+      <Route path="/Menu" element={<Menu />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")

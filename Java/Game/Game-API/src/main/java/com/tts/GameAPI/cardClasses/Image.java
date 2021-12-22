8e224c992941972e72f7409b545eb84a11f8d2d1
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-12-21 22:38:54 -0600

    ➕ Removed extra getters

--- Java/Game/Game-API/src/main/java/com/tts/GameAPI/cardClasses/Image.java
+++ Java/Game/Game-API/src/main/java/com/tts/GameAPI/cardClasses/Image.java
@@ -1,13 +1,5 @@
 package com.tts.GameAPI.cardClasses;
 
 public record Image(String src, String alt) {
-
-    public String getSrc() {
-        return src;
-    }
-
-    public String getAlt() {
-        return alt;
-    }
 }
 

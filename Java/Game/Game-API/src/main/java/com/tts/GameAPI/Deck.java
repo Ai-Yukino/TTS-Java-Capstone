b9082578c82dde5ebcfe8e9ffcfadd810f52e774
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-12-21 22:39:43 -0600

    🚧 Started deck methods

--- Java/Game/Game-API/src/main/java/com/tts/GameAPI/Deck.java
+++ Java/Game/Game-API/src/main/java/com/tts/GameAPI/Deck.java
@@ -3,19 +3,30 @@ package com.tts.GameAPI;
 // 📝 TODO: Deck class for game calculations only!
 // The deck will hold exactly n * 52 cards in a random order where $n\in\mathbb{N}$. In production, the react app in `react-meta-component` will only be sent individual cards rather than a deck.
 
+import com.tts.GameAPI.cardClasses.Image;
+import com.tts.GameAPI.cardClasses.Ranks;
+import com.tts.GameAPI.cardClasses.Suits;
+
 import java.util.ArrayList;
 
 public class Deck {
 
     int n;
-
     private ArrayList<Card> cardPool = new ArrayList<>();
-    private static final String[] suitPool = new String[]{"Clubs", "Diamonds", "Hearts", "Spades"};
-    private static final String[] rankPool = new String[]{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
-
     private ArrayList<Card> cards = new ArrayList<>();
 
     // 📝 Constructs deck of 52 cards "in order"
     public Deck() {
+        n = 1;
+        int numCards = 0;
+        Suits suits[] = Suits.values();
+        Ranks ranks[] = Ranks.values();
+
+        for (Suits suit : suits) {
+            for (Ranks rank : ranks) {
+                numCards++;
+                // cardPool.add()
+            }
+        }
     }
 }

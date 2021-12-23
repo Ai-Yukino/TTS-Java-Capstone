9a4d4c524695c5a9deea0abf3011dfbd6753e3d7
Cal Krug <cal.krug@protonmail.com>
2021-12-22 19:39:56 -0600

    updates on styles and (potential) routing

--- React/react-meta-component/src/routes/Menu.js
+++ React/react-meta-component/src/routes/Menu.js
@@ -1,12 +1,33 @@
 import '../css/atomic-DEFCON2.css';
+import {Link} from 'react-router-dom';
+
 export default function Menu() {
   return (
-    <div className={
-      "display-center" +
+    <div className="position-center-flex">
+      <div className={
+      "colorBG-charcoal" +
       " " +
-      "colorBG-charcoal"
-      }>
-      MenuItem
+      "colorBorder-darkGreen" +
+      " " +
+      "dimension-div-menu" +
+      " " +
+      "position-center-flex"}>
+        <div className={
+        "colorBG-darkBrown" +
+        " " +
+        "colorBorderBottom-darkGreen" +
+        " " +
+        "dimension-div-menuBar" +
+        " " +
+        "position-top-flex" +
+        " " +
+        "positionTest-center-flex"}>
+          <h1 className="positionTest-center-flex">
+            Foo
+          </h1>
+        </div>
+        <Link to={<Game />}>Log In</Link>
+      </div>
     </div>
   );
 }
